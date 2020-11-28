7c9503c0479a5e5a4c16db0e0c98aff78ba6c2fa
E2295-Tuncay <tuncayermangm@gmail.com>
2020-11-28 21:42:03 +0300

    Typography added

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Main from "../pages/Main";
 import Signup from "../pages/Signup";
 import Signin from "../pages/Signin";
+import UserDetail from "../pages/UserDetail";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -10,8 +12,10 @@ function AppRouter() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/register" component={Signup} />
-        <Route path="/login" component={Signin} />
+        <Route exact path="/register" component={Signup} />
+        <Route exact path="/login" component={Signin} />
+        <Route exact path="/user/:id" component={UserDetail} />
+        <Route path="/" component={Main} />
       </Switch>
       <Footer />
     </Router>
